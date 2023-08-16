2ae5c7435ea05206507dd42a76107c83abebc349
Olivier Gorzalka <olivier@amphibee.fr>
2023-08-16 11:49:02 +0200

    Hooks : Refactor some sections + added hookable class

--- src/Hook/IHook.php
+++ src/Hook/IHook.php
@@ -9,46 +9,36 @@ interface IHook
     /**
      * Add event using the WordPress hooks.
      *
-     * @param  string|array  $hooks         The hook name.
-     * @param  \Closure|string|array  $callback      Using a class method like so "MyClass@method"
-     * @param  int  $priority
-     * @param  int  $accepted_args
-     * @return mixed
+     * @param  string  $hooks The hook name.
+     * @param  array|string|\Closure  $callback Using a class method like so "MyClass@method"
      */
-    public function add($hooks, $callback, $priority = 10, $accepted_args = 2);
+    public function add(string $hooks, array|string|\Closure $callback, int $priority = 10, int $accepted_args = 2): mixed;
 
     /**
      * Run all events registered with the hook.
      *
      * @param  string  $hook The event hook name.
-     * @param  mixed  $args
-     * @return mixed
      */
-    public function run($hook, $args = null);
+    public function run(string $hook, mixed $args = null): mixed;
 
     /**
      * Check if a registered hook exists.
-     *
-     * @param  string  $hook
-     * @return bool
      */
-    public function exists($hook);
+    public function exists(string $hook): bool;
 
     /**
      * Return the callback registered with the given hook.
      *
      * @param  string  $hook The hook name.
-     * @return array|bool
      */
-    public function getCallback($hook);
+    public function getCallback(string $hook): ?array;
 
     /**
      * Remove a defined action or filter.
      *
      * @param  string  $hook     The hook name.
-     * @param  \Closure|string  $callback The callback to remove.
+     * @param  string|\Closure|null  $callback The callback to remove.
      * @param  int  $priority The priority number.
-     * @return mixed
      */
-    public function remove($hook, $callback = null, $priority = 10);
+    public function remove(string $hook, string|\Closure $callback = null, int $priority = 10): mixed;
 }
