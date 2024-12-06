878f74c3e7764a1b66bb13a094f30a441b2fad0e
Olivier Gorzalka <olivier@amphibee.fr>
2024-12-06 12:55:54 +0100

    ✨ (gitignore, AssetContainerFactory.php, AssetManager.php): add .gitignore file to exclude unnecessary files and directories ✨ (AssetContainerFactory.php, AssetManager.php): introduce AssetContainerFactory and AssetManager classes to manage assets effectively

---
+++ src/Asset/AssetManager.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollora\Asset;
+
+class AssetManager
+{
+    public function __construct(
+        private AssetFactory $factory,
+        private AssetContainerManager $containerManager
+    ) {}
+
+    public function add(string $handle, string $file): Asset
+    {
+        return $this->factory->add($handle, $file);
+    }
+
+    public function container(?string $name = null): AssetContainer
+    {
+        return $this->containerManager->container($name);
+    }
+}
