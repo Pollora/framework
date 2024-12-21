98fe15aad9493aed8664ae2d5d6da53ab6c52396
Olivier Gorzalka <olivier@amphibee.fr>
2024-12-21 09:12:07 +0100

    📝 (Application.php, Bootstrap.php): add documentation comments and new method to check WP-CLI environment
    ✨ (Application.php, Bootstrap.php): implement runningInWpCli method to determine if the application is running in the WordPress CLI environment
    
    ♻️ (Bootstrap.php): refactor conditions to improve clarity and ensure proper handling of WP-CLI and console environments

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -10,10 +10,30 @@ use Pollora\Route\RouteServiceProvider;
 
 class Application extends \Illuminate\Foundation\Application
 {
+    /**
+     * Register the base service providers for the application.
+     *
+     * This method ensures that essential Laravel services (Events, Logging, Routing) are available.
+     *
+     * @return void
+     */
     public function registerBaseServiceProviders(): void
     {
         $this->register(new EventServiceProvider($this));
         $this->register(new LogServiceProvider($this));
         $this->register(new RouteServiceProvider($this));
+    }
+
+    /**
+     * Determine if the application is running in the WP-CLI environment.
+     *
+     * This checks whether the WP_CLI constant is defined and evaluates to true,
+     * indicating that the application is being executed within the WordPress CLI.
+     *
+     * @return bool True if running in WP-CLI, false otherwise.
+     */
+    public function runningInWpCli(): bool
+    {
+        return defined('WP_CLI') && WP_CLI;
     }
 }
