735c44fb28792758fbbe75aac1bed5c99ffc17c1
Olivier Gorzalka <olivier@amphibee.fr>
2025-06-23 17:52:17 +0200

    Fix php 8.2 error

--- src/Theme/Infrastructure/Services/WordPressThemeAdapter.php
+++ src/Theme/Infrastructure/Services/WordPressThemeAdapter.php
@@ -15,9 +15,9 @@ use Pollora\Theme\Domain\Contracts\WordPressThemeInterface;
  */
 class WordPressThemeAdapter implements WordPressThemeInterface
 {
-    public const string DEFAULT_THEME_NAME = 'default';
+    public const DEFAULT_THEME_NAME = 'default';
 
-    public const string DEFAULT_URI = '/';
+    public const DEFAULT_URI = '/';
 
     /**
      * Check if WordPress is in installation mode.
