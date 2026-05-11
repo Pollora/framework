9b824ef0faeb8aaf6ed5be1060f4f3907d95edbd
Olivier Gorzalka <olivier@amphibee.fr>
2026-05-11 12:33:15 +0200

    fix: restore ContainerInterface type in ThemeRegistrar constructor
    ThemeServiceProvider passes a DomainContainerAdapter (PSR container)
    to ThemeRegistrar. The cleanup-4 commit incorrectly changed the type
    to Illuminate\Contracts\Container\Container, causing a TypeError at boot.

--- src/Theme/Application/Services/ThemeRegistrar.php
+++ src/Theme/Application/Services/ThemeRegistrar.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Pollora\Theme\Application\Services;
 
-use Illuminate\Contracts\Container\Container as ContainerContract;
+use Psr\Container\ContainerInterface;
 use Illuminate\Foundation\Application;
 use Pollora\BlockPattern\UI\PatternComponent;
 use Pollora\Modules\Domain\Contracts\ModuleDiscoveryOrchestratorInterface;
@@ -32,7 +32,7 @@ class ThemeRegistrar implements ThemeRegistrarInterface
     private ?ThemeModuleInterface $activeTheme = null;
 
     public function __construct(
-        protected ContainerContract $app,
+        protected ContainerInterface $app,
         protected WordPressThemeParser $themeParser
     ) {}
 
